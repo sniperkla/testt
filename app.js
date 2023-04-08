11988344ab623b887354dd012f1ed1ad79dc60c3
Your Name <you@example.com>
2023-04-08 22:29:41 +0700

    sss

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello, world!");
 });
 
