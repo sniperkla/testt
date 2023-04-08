f80622e8db55c573609726280596fe6180298851
Your Name <you@example.com>
2023-04-08 23:21:18 +0700

    sss

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const bodyparser = require("body-parser");
 const app = express();
 app.use(cors());
-
+app.use(bodyparser.json());
 app.get("/test", (req, res) => {
   res.send("Hello, world!");
 });
 
 app.listen(4000, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port 4000");
 });
